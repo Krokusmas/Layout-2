b31e80881c9f6874e2886fd33485816c9348b0f3
Krokusmas <anton.sedchenko@gmail.com>
2020-09-20 03:49:03 +0300

    testimonial section

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ jQuery(function(){
         }
     });
 });
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    center:true,
+    mouseDrag:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
